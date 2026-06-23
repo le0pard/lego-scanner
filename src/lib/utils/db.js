f4a18501405774c2a3b9ddecbcd46bbfd656ed59
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-06-23 16:44:11 +0300

    cleanup

--- src/lib/utils/db.js
+++ src/lib/utils/db.js
@@ -8,23 +8,3 @@ db.version(DB_VERSION).stores({
   minifigures: '&slug, series, *searchKeys',
   syncMeta: '&series'
 });
-
-// export async function syncDatabase() {
-//   const MASTER_JSON_URL =
-//     'https://raw.githubusercontent.com/YOUR_USER/YOUR_REPO/main/static/db.json';
-
-//   try {
-//     const response = await fetch(MASTER_JSON_URL);
-//     if (!response.ok) throw new Error('Network offline or repository unavailable.');
-
-//     const data = await response.json();
-
-//     // Bulk put handles upserts gracefully (updates match, adds new rows)
-//     await db.minifigures.bulkPut(data.figures);
-//     console.log('IndexedDB synchronized with cloud master file.');
-//     return true;
-//   } catch (error) {
-//     console.warn('Network sync failed. Running on local cache.', error.message);
-//     return false;
-//   }
-// }
