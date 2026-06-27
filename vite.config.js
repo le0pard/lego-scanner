ae562237a3e135df2b8c61d6d8bb9164130c54cf
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-06-28 01:40:33 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -29,5 +29,10 @@ export default defineConfig({
   ],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version)
+  },
+  build: {
+    target: 'es2022',
+    sourcemap: false,
+    reportCompressedSize: false
   }
 });
