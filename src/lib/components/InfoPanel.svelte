2681241b73e09f17f929a764444ca3a18ea12aad
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-09-24 18:51:19 +0300

    fix scaling

--- src/lib/components/InfoPanel.svelte
+++ src/lib/components/InfoPanel.svelte
@@ -4,7 +4,7 @@
   import { syncState } from '$lib/states/sync.svelte.js';
   import { formatDateTime } from '$lib/utils/date.js';
   import { triggerDatabaseSync } from '$lib/utils/sync_manager.js';
-  import dataMatrixCodeExampleImg from '$lib/assets/howto/data-matrix-example.jpg?enhanced&w=360;480;720';
+  import dataMatrixCodeExampleImg from '$lib/assets/howto/data-matrix-example.jpg?enhanced&w=360;480;650';
 
   // Prevent multiple clicks while syncing
   let isManualSyncing = $state(false);
